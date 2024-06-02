dc725674fc158f42ef2493721a6bb1bb6fd9f315
KendryLeX <162934264+KendryLeX@users.noreply.github.com>
2024-06-02 19:53:17 -0400

    Add files via upload

---
+++ proyectos/scipt-proyectos.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 0,
+    mousewheel: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  
